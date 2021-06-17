c659c7e2b66d36e315ba994acb0ad5c254d54f50
Pekaby <11gavegames11@gmail.com>
2021-06-17 23:54:16 +0300

    Update download.php

--- temp/language/en/temp/download.php
+++ temp/language/en/temp/download.php
@@ -33,26 +33,7 @@ tmpheader('thx_for_download.css');
           <span class="sp">></span><a href="/" class="link">Main page</a>
         </div>
         <div class="ad_block">
-          <!-- Yandex.RTB R-A-571623-6 -->
-<div id="yandex_rtb_R-A-571623-6"></div>
-<script type="text/javascript">
-    (function(w, d, n, s, t) {
-        w[n] = w[n] || [];
-        w[n].push(function() {
-            Ya.Context.AdvManager.render({
-                blockId: "R-A-571623-6",
-                renderTo: "yandex_rtb_R-A-571623-6",
-                async: true
-            });
-        });
-        t = d.getElementsByTagName("script")[0];
-        s = d.createElement("script");
-        s.type = "text/javascript";
-        s.src = "//an.yandex.ru/system/context.js";
-        s.async = true;
-        t.parentNode.insertBefore(s, t);
-    })(this, this.document, "yandexContextAsyncCallbacks");
-</script>
+         
         </div>
   </div>
 </main>
